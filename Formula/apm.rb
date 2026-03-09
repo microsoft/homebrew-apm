20467efb231bffc645aedf16ad856a1f7d0580b5
GitHub Action <action@github.com>
2026-03-09 20:35:11 +0000

    Update APM formula to v0.7.5

--- Formula/apm.rb
+++ Formula/apm.rb
@@ -1,21 +1,21 @@
 class Apm < Formula
   desc "Agent Package Manager (APM): The NPM for AI-Native Development"
   homepage "https://github.com/microsoft/apm"
-  version "0.7.4"
+  version "0.7.5"
   license "MIT"
 
   if Hardware::CPU.arm? && OS.mac?
     url "https://github.com/microsoft/apm/releases/download/v#{version}/apm-darwin-arm64.tar.gz"
-    sha256 "822e680f06ffae878b807c8303bf0856ed5e2d3b7f7f8959b4482699530d2d8f"
+    sha256 "5a5d771376d309a35350358f15e4171a4f7b01c9deae955c69f094b8810faae9"
   elsif Hardware::CPU.intel? && OS.mac?
     url "https://github.com/microsoft/apm/releases/download/v#{version}/apm-darwin-x86_64.tar.gz"
-    sha256 "a835cd5f90bd0e500acfb8096531ad9245fb8704c02380f0afcdee3928c1a8b0"
+    sha256 "6c7ab38caf7114533f157722a6e3afc3306da366e080b7877d43235f3173be75"
   elsif Hardware::CPU.arm? && OS.linux?
     url "https://github.com/microsoft/apm/releases/download/v#{version}/apm-linux-arm64.tar.gz"
-    sha256 "7b543e4daf7bc029513f9eb7bbfa4240b3e1f748ffa08f243ab2f7532e7adb53"
+    sha256 "dcf07e1323b5914731048c1eb79d4cdb373765503d76b57f1a589483b9f5d62a"
   elsif Hardware::CPU.intel? && OS.linux?
     url "https://github.com/microsoft/apm/releases/download/v#{version}/apm-linux-x86_64.tar.gz"
-    sha256 "adf8f6ba5a8694e998fbbaf1aa7490f5bb17c75dc1683ca5825e02c325fb96cf"
+    sha256 "0786e77b590a4b9c7d6d7625c68e98466667d98be988c5e607517a66eb4ce624"
   end
 
   def install
